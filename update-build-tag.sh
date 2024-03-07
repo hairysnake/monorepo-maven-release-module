8edf3d0b3669600e9227f500e3d5d48b20cdd706
Tomasz Dluzniewski <hairysnake76@gmail.com>
2024-03-07 04:32:43 -0500

    new version of script

--- update-build-tag.sh
+++ update-build-tag.sh
@@ -8,13 +8,24 @@ for folder in */; do
     tag=
 
     if [ -n "$commits" ]; then
+
         echo "new commits in $module_name"
+        echo "step 0 - revert previous changes if needed ... "
+
+        if [ -e "$module_name/pom.xml.versionsBackup" ]; then
+            mvn versions:revert -pl $module_name
+        else
+            echo "pom.xml.versionsBackup file not found, no changes to revert."
+        fi
+    
+        echo ""
         echo -n "step 1 - current version evaluation ... "
         
         snapshot_version=$(mvn --color never help:evaluate -Dexpression=project.version -q -DforceStdout -f $module_name)
         echo "$snapshot_version"
         release_version=$(echo "$snapshot_version" | sed "s/-SNAPSHOT//" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g")
 
+        echo ""
         echo "step 2 - releasing version $release_version for $module_name"
         echo ""
         mvn versions:set -pl $module_name -DnewVersion=$release_version
@@ -27,12 +38,13 @@ for folder in */; do
         echo ""
         echo "step 4 - commit version $release_version for $module_name"
         echo ""
+        git add $module_name/pom.xml
         git commit -m "$module_name version $release_version"
         
         new_snapshot_version="$release_version-SNAPSHOT"
-        
+
         echo "step 4 - releasing new snapshot version $new_snapshot_version for $module_name"
         echo ""
-        mvn versions:set -pl $module_name -DnewVersion=$new_snapshot_version
+        mvn versions:nextSnapshot -pl $module_name
     fi
 done
