8f3e4df4bdc841b6bc0a6447713b790005167643
Tomasz Dluzniewski <hairysnake76@gmail.com>
2024-03-18 13:52:06 -0400

    version with common libraries

--- update-build-tag.sh
+++ update-build-tag.sh
@@ -1,50 +1,60 @@
 #!/bin/bash
 
-last_tag=$(git describe --tags --abbrev=0)
+# here define common libraries
+libraries=("library-a" "library-b")
+
+revert_version() {
+    if [ -e "$1/pom.xml.versionsBackup" ]; then
+        echo "reverting uncommited changes in $1"
+        mvn versions:revert -pl $1
+    fi
+}
+
+#
+# revert uncommited changes (previous execution failed)
+#
 
 for folder in */; do
     module_name=${folder%/}
+    revert_version $module_name
+done
+
+#
+# get last tag from git
+#
+
+last_tag=$(git describe --tags --abbrev=0)
+
+#
+# build common libraries
+#
+
+for folder in */; do
+    module_name=${folder%/}
+    is_library=false
+
+    for library in "${libraries[@]}"; do
+        if [ "$module_name" == "$library" ]; then
+            is_library=true
+        fi
+    done
+
+    if [ $is_library == true ]; then
+        commits=$(git log $last_tag..HEAD -- $module_name)
+        if [ -n "$commits" ]; then    
+            echo "$commits"
+        fi
+    fi
+done
+
+#
+# build modules
+#
+for folder in */; do
+    module_name=${folder%/}
     commits=$(git log $last_tag..HEAD -- $module_name)
-    tag=
 
     if [ -n "$commits" ]; then
-
-        echo "new commits in $module_name"
-        echo "step 0 - revert previous changes if needed ... "
-
-        if [ -e "$module_name/pom.xml.versionsBackup" ]; then
-            mvn versions:revert -pl $module_name
-        else
-            echo "pom.xml.versionsBackup file not found, no changes to revert."
-        fi
-    
-        echo ""
-        echo -n "step 1 - current version evaluation ... "
-        
-        snapshot_version=$(mvn --color never help:evaluate -Dexpression=project.version -q -DforceStdout -f $module_name)
-        echo "$snapshot_version"
-        release_version=$(echo "$snapshot_version" | sed "s/-SNAPSHOT//" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[mGK]//g")
-
-        echo ""
-        echo "step 2 - releasing version $release_version for $module_name"
-        echo ""
-        mvn versions:set -pl $module_name -DnewVersion=$release_version
-        
-        echo ""
-        echo "step 3 - building version $release_version for $module_name"
-        echo ""
-        mvn clean install -pl $module_name
-
-        echo ""
-        echo "step 4 - commit version $release_version for $module_name"
-        echo ""
-        git add $module_name/pom.xml
-        git commit -m "$module_name version $release_version"
-        
-        new_snapshot_version="$release_version-SNAPSHOT"
-
-        echo "step 4 - releasing new snapshot version $new_snapshot_version for $module_name"
-        echo ""
-        mvn versions:nextSnapshot -pl $module_name
+        echo "changes"
     fi
 done
